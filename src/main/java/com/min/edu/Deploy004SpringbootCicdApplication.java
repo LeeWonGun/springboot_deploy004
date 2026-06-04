b341593148e0a678fb9477134f68752c427a92f8
LeeWonGun <wongun1850@gmail.com>
2026-06-04 09:46:38 +0900

    feet: 2026.06.04 SpringBoot를 배포 with Docker

---
+++ src/main/java/com/min/edu/Deploy004SpringbootCicdApplication.java
@@ -0,0 +1,13 @@
+package com.min.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deploy004SpringbootCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Deploy004SpringbootCicdApplication.class, args);
+    }
+
+}
